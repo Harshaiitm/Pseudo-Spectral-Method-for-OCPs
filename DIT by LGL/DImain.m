2231174940727855e9352d0d93b21e82b961ed85
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2023-08-01 23:57:01 +0530

    Update DImain.m

--- DIT by LGL/DImain.m
+++ DIT by LGL/DImain.m
@@ -98,42 +98,38 @@ legend({'control variable'},Location="northeast");
 title('Double integrator tracking problem');
 
 %% Polynomial
+clc;
 z_value = 0.5;
+disp(['at time t =',num2str(z_value),'s']);
 coeff_P = polyfit(t,x1,N);
 sympref('FloatingPointOutput',true);
 syms z
-polynomial = sym(0);
+polynomial_p = sym(0);
 for i= 1:N+1
-    polynomial =vpa(polynomial+coeff_P(i)*z^(N-i+1));
+    polynomial_p =vpa(polynomial_p+coeff_P(i)*z^(N-i+1));
 end
 % polynomial = polyval(coeff', z);
-disp(['Position Equation:', char(polynomial)]);
-position = subs(polynomial,z,z_value);
-disp(['Position:', char(position)]);
+disp(['Position Equation:', char(polynomial_p)]);
+position = subs(polynomial_p,z,z_value);
+disp(['Position:', char(position),'m']);
 
 coeff_V = polyfit(t,x2,N);
-sympref('FloatingPointOutput',true);
-syms z
-polynomial = sym(0);
+polynomial_V = sym(0);
 for i= 1:N+1
-    polynomial =vpa(polynomial+coeff_V(i)*z^(N-i+1));
+    polynomial_V =vpa(polynomial_V+coeff_V(i)*z^(N-i+1));
 end
-% polynomial = polyval(coeff', z);
-disp(['Velocity Equation:', char(polynomial)]);
-velocity = subs(polynomial,z,z_value);
-disp(['velocity:', char(velocity)]);
+disp(['Velocity Equation:', char(polynomial_V)]);
+velocity = subs(polynomial_V,z,z_value);
+disp(['velocity:', char(velocity),'m/s']);
 
 coeff_A = polyfit(t,x3,N);
-sympref('FloatingPointOutput',true);
-syms z
-polynomial = sym(0);
+polynomial_A = sym(0);
 for i= 1:N+1
-    polynomial =vpa(polynomial+coeff_A(i)*z^(N-i+1));
+    polynomial_A =vpa(polynomial_A+coeff_A(i)*z^(N-i+1));
 end
-% polynomial = polyval(coeff', z);
-disp(['Acceleration Equation:', char(polynomial)]);
-accleration = subs(polynomial,z,z_value);
-disp(['Acceleration:', char(accleration)]);
+disp(['Acceleration Equation:', char(polynomial_A)]);
+accleration = subs(polynomial_A,z,z_value);
+disp(['Acceleration:', char(accleration),'m/s^2']);
 
 
 
