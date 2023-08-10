7bc2fa61de987c5d793668b644cab99823362954
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2023-08-11 01:30:17 +0530

    Delete Double integrator tracking problem directory

--- Double integrator tracking problem/Objectivee_func_LGR.m
+++
@@ -1,9 +0,0 @@
-function Objective = Objectivee_func_LGR(x,N,weights,t)
-x1 = x(1:N+1);
-x2 = x(N+2:2*N+2);
-x3 = x(2*N+3:3*N+3);
-xr = 5*sin(t');
-v = 5*cos(t');
-
-Objective = @(x) sum(((x1-xr).*(x1-xr))'.*weights+((x2-v).*(x2-v))'.*weights+(x3.*x3)'.*weights);
-end
