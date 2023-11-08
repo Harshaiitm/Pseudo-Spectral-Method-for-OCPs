5a1964954e081dda2fd689abc611b0c25909dbbf
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2023-11-08 15:53:21 +0530

    Add files via upload

--- ND_Normal_Tangential_Polar_Rocket_problem/ND_Normal_Tangential_Polar_Rocket_Nonlinear_func_LGL.m
+++ ND_Normal_Tangential_Polar_Rocket_problem/ND_Normal_Tangential_Polar_Rocket_Nonlinear_func_LGL.m
@@ -43,21 +43,17 @@ Isp = Isp*n_time;
 h =  R/n_length - Re;
 rho = rho0 * exp(-(h./h_scale));
 g = mu./(R./n_length).^2;
-g0 = mu/Re;
+g0 = mu/Re^2;
 
 q = 0.5*rho.*(V/n_velocity).^2;
 Drag = q.* A_ref *CD;
 Drag_ref = m0*g0;
 Drag = Drag./Drag_ref;
-size(Drag)
+
 
 a_sen_v = (Thrust.* cos(alpha) - Drag)./(mass);
 a_sen_gamma = (Thrust.* sin(alpha))./(mass);
 a_sen_mag = sqrt(a_sen_v.^2 + a_sen_gamma.^2);
-
-K = 0.5*rho0*Re^3*CD/m0;
-size(K)
-
 
 
 c = zeros(2*N+2,1);
@@ -67,8 +63,8 @@ c(N+2:2*N+2,1) = (q - q_max)* 0.5 * rho0 * (mu/Re);
 ceq = zeros(5*N+5,1);
 ceq(1:N+1,1) = D*R' - ((final_time-t0)/2)*(V.*sin(gamma))';
 ceq(N+2:2*N+2,1) = D*theta' - ((final_time-t0)/2)*(V.*cos(gamma)./R)';
-ceq(2*N+3:3*N+3,1) = D*V' - ((final_time-t0)/2)*((Thrust.*cos(alpha)./mass - K*Drag./mass) - (sin(gamma)./R.^2))';
-ceq(3*N+4:4*N+4,1) = D*gamma' - ((final_time-t0)/2)*((Thrust.*sin(alpha)./(mass.*V)) - ((V.^2./R)-(1/R.^2)).*(cos(gamma)./V))';
+ceq(2*N+3:3*N+3,1) = D*V' - ((final_time-t0)/2)*((Thrust.*cos(alpha)./mass - Drag./mass) - (sin(gamma)./R.^2))';
+ceq(3*N+4:4*N+4,1) = D*gamma' - ((final_time-t0)/2)*((Thrust.*sin(alpha)./(mass.*V)) - ((V.^2./R)-(1./R.^2)).*(cos(gamma)./V))';
 ceq(4*N+5:5*N+5,1) = D*mass' + ((final_time-t0)/2)*(Thrust./Isp)';
 
 end
