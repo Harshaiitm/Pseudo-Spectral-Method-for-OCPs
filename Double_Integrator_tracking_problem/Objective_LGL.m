f1f959cc02aaa6c170db21e80586247a6d9ed1a0
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2024-03-08 17:29:17 +0530

    Delete Double_Integrator_tracking_problem/Objective_LGL.m

--- Double_Integrator_tracking_problem/Objective_LGL.m
+++
@@ -1,9 +0,0 @@
-function Objective = Objective_LGL(x,M,weights,t0,tf,t)
-x1 = x(1:M);
-x2 = x(M+1:2*M);
-x3 = x(2*M+1:3*M);
-xr = 5*sin(t');  % referece for position to follow 
-vr = 5*cos(t');   % referece for velocity to follow   
-
-Objective = ((tf-t0)/2)*(sum(((x1-xr).*(x1-xr))'.*weights+ ((x2-vr).*(x2-vr))'.*weights+(x3.*x3)'.*weights*0.0001));
-end 
