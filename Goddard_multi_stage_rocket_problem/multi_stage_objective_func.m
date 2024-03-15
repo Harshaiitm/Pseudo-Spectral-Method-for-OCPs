e05f25dabbec2d42ac32a4561dd9b4de0c37507a
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2024-03-15 14:07:54 +0530

    Delete Goddard_multi_stage_rocket_problem directory

--- Goddard_multi_stage_rocket_problem/multi_stage_objective_func.m
+++
@@ -1,4 +0,0 @@
-function Objective = multi_stage_objective_func(x,M)
-h_f = x(5*M);        
-Objective = -h_f;
-end
--- Goddard_multi_stage_rocket_problem/single_stage_objective_func.m
+++
@@ -1,4 +0,0 @@
-function Objective = single_stage_objective_func(x,M)
-h_f = x(M);        
-Objective = -h_f;
-end
