598b3c4ba6596a9b327c3316887ed757e1796a10
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2024-05-08 13:00:52 +0530

    Delete ND_Normal_Tangential_Polar_Rocket_problem directory

--- ND_Normal_Tangential_Polar_Rocket_problem/ND_Normal_Tangential_Polar_Rocket_objective_func.m
+++
@@ -1,6 +0,0 @@
-function Objective =ND_Normal_Tangential_Polar_Rocket_objective_func(x,M,m0,n_mass)
-% mass(end) = x(5*M);
-mp = m0*n_mass - x(5*M);
-F = mp/(m0*n_mass);        
-Objective = F;
-end
