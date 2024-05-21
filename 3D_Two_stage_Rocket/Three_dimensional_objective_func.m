190cd30785ee9326d8af27d1300e2860696a26bc
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2024-05-21 11:12:11 +0530

    Add files via upload

---
+++ 3D_Two_stage_Rocket/Three_dimensional_objective_func.m
@@ -0,0 +1,11 @@
+function Objective = Three_dimensional_objective_func(x,M,problem)
+
+m0 = problem.m0;
+m0_2 = problem.m0_2;
+
+% mp1 = m0 - (x(7*M));
+mp2 = m0 - (x(21*M));
+% F1 = mp1/m0;
+F = mp2/m0;
+Objective = F;
+end
