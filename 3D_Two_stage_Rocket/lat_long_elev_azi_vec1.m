8fb94febc03318c479f297fbeecdd070ef28e278
Harshaiitm <119575305+Harshaiitm@users.noreply.github.com>
2024-06-07 14:13:30 +0530

    Updated for quaternion elements

--- 3D_Two_stage_Rocket/lat_long_elev_azi_vec1.m
+++ 3D_Two_stage_Rocket/lat_long_elev_azi_vec1.m
@@ -1,13 +1,5 @@
 function [R1_I,V1_I,qn1] = lat_long_elev_azi_vec1(M,problem) 
 
-Elev_i = problem.Elev_i;
-Azim_i = problem.Azim_i;
-hi = problem.hi;
-Vi = problem.Vi;
-Elev_s = problem.Elev_s;
-Azim_s = problem.Azim_s;
-hf_s = problem.hf_s;
-Vf_s = problem.Vf_s;
 Re = problem.Re;
 nodes = problem.nodes;
 D = problem.D;
@@ -15,11 +7,17 @@ D = problem.D;
 t0 = 0;
 stage_time = 160;
 
+% Earth's rotational velocity in the inertial frame
+Omega_z = 2 * pi / (24 * 60 * 60);  % Sidereal Rotation Rate (rad/s)
+Omega_x = 0;
+Omega_y = 0;
+Omega_I = [Omega_x; Omega_y; Omega_z];
+
 load time_alt_lat_long.csv
-time = time_alt_lat_long(1:9,1);
-alt = time_alt_lat_long(1:9,2)*1000;
-lat = time_alt_lat_long(1:9,3);
-long = time_alt_lat_long(1:9,4);
+time = time_alt_lat_long(:,1);
+alt = time_alt_lat_long(:,2)*1000;
+lat = time_alt_lat_long(:,3);
+long = time_alt_lat_long(:,4);
 
 t_1 = ((stage_time-t0)/2).*nodes+(stage_time+t0)/2;
 
@@ -27,77 +25,72 @@ Altitude0_1 = interp1(time, alt, t_1, 'pchip');
 latitude_1 = deg2rad(interp1(alt, lat, Altitude0_1, 'pchip'));
 longitude_1 = deg2rad(interp1(alt, long, Altitude0_1, 'pchip'));
 
-Elev_1 = linspace(Elev_i,Elev_s,M);
-Azim_1 = linspace(Azim_i,Azim_s,M);
-
 theta_1 = pi/2 - latitude_1;
 phi_1 = longitude_1;
 
 % Radial Distance from Earth's center
-R1_B = (Altitude0_1' + Re);
+R1_B = (Altitude0_1 + Re);
+
+% R1_B = ((R1_B(end)-R1_B(1))/2).*nodes+(R1_B(end)+R1_B(1))/2;
+theta_1 = ((theta_1(end)-theta_1(1))/2).*nodes+(theta_1(end)+theta_1(1))/2;
+phi_1 = ((phi_1(end)-phi_1(1))/2).*nodes+(phi_1(end)+phi_1(1))/2;
 
 % Cartesian Coordinates
-Rx1_I = R1_B' .* sin(theta_1) .* cos(phi_1);
-Ry1_I = R1_B' .* sin(theta_1) .* sin(phi_1);
-Rz1_I = R1_B' .* cos(theta_1);
+Rx1_I = R1_B .* sin(theta_1) .* cos(phi_1);
+Ry1_I = R1_B .* sin(theta_1) .* sin(phi_1);
+Rz1_I = R1_B .* cos(theta_1);
 
 R1_I = [Rx1_I'; Ry1_I'; Rz1_I'];
 
 R1_I_mag = sqrt(Rx1_I.^2 + Ry1_I.^2 + Rz1_I.^2);
 % disp(R1_I_mag);
 
-% Earth's rotational velocity in the inertial frame
-Omega_z = 2 * pi / (24 * 60 * 60);  % Sidereal Rotation Rate (rad/s)
-Omega_x = 0;
-Omega_y = 0;
-Omega_I = [Omega_x; Omega_y; Omega_z];
-
-% Velocity components in spherical coordinates
-V1_r =  (2/160)*(D*R1_B');
-V1_theta = 0;
-V1_phi = Omega_z*sin(theta_1)*(Re+hi);
-
-% Transformation from spherical to Cartesian coordinates
-Vx1_I = V1_r .* sin(theta_1) .* cos(phi_1) + V1_theta.* cos(theta_1) .* cos(phi_1) - V1_phi.* sin(phi_1);
-Vy1_I = V1_r .* sin(theta_1) .* sin(phi_1) + V1_theta.* cos(theta_1) .* sin(phi_1) + V1_phi.* cos(phi_1);
-Vz1_I = V1_r .* cos(theta_1) - V1_theta.* sin(theta_1);
+Vx1_I = (2/160)*(D*Rx1_I);
+Vy1_I = (2/160)*(D*Ry1_I);
+Vz1_I = (2/160)*(D*Rz1_I);
 
 V1_I = [Vx1_I';Vy1_I'; Vz1_I'];
-
 V1_I_mag = sqrt(Vx1_I.^2 + Vy1_I.^2 + Vz1_I.^2);
-% disp(V1_I_mag);
-
-latitude_1 = deg2rad(28);
-longitude_1 = deg2rad(0);
 
 
-% Attitude matrix
-Q11 = - sin(latitude_1).*cos(longitude_1).*cos(Elev_1).*cos(Azim_1) - sin(longitude_1).*cos(Elev_1).*sin(Azim_1) + cos(latitude_1).*cos(longitude_1).*sin(Elev_1);
+Q11 = zeros(M, 1); 
+Q12 = zeros(M, 1);
+Q13 = zeros(M, 1);
+Q21 = zeros(M, 1); 
+Q22 = zeros(M, 1);
+Q23 = zeros(M, 1);
+Q31 = zeros(M, 1); 
+Q32 = zeros(M, 1);
+Q33 = zeros(M, 1);
 
-Q12 = sin(latitude_1).*cos(longitude_1).*sin(Azim_1) - sin(longitude_1).*cos(Azim_1);
-
-Q13 = - sin(latitude_1).*cos(longitude_1).*sin(Elev_1).*cos(Azim_1) - sin(longitude_1).*sin(Elev_1).*sin(Azim_1) - cos(latitude_1).*cos(longitude_1).*cos(Elev_1);
-
-Q21 = - sin(latitude_1).*sin(longitude_1).*cos(Elev_1).*cos(Azim_1) + cos(longitude_1).*cos(Elev_1).*sin(Azim_1) + cos(latitude_1).*sin(longitude_1).*sin(Elev_1);
-
-Q22 = sin(latitude_1).*sin(longitude_1).*sin(Azim_1) + cos(longitude_1).*cos(Azim_1);
-
-Q23 = - sin(latitude_1).*sin(longitude_1).*sin(Elev_1).*cos(Azim_1) + cos(longitude_1).*sin(Elev_1).*sin(Azim_1) - cos(latitude_1).*sin(longitude_1).*cos(Elev_1);
-
-Q31 = cos(latitude_1).*cos(Elev_1).*cos(Azim_1) + sin(latitude_1).*sin(Elev_1);
-
-Q32 = - cos(latitude_1).*sin(Azim_1);
-
-Q33 = cos(latitude_1).*sin(Elev_1).*cos(Azim_1) - sin(latitude_1).*cos(Elev_1);
-
+for i = 1:M
+    V_I1 = [Vx1_I(i); Vy1_I(i); Vz1_I(i)];
+    V1_R = (2/160)*D(i,:)*R1_I_mag;
+    V1_theta = (2/160)*R1_I_mag(i).*(D(i,:)*theta_1);
+    V1_phi = (2/160)*R1_I_mag(i).*sin(theta_1(i)).*(D(i,:)*phi_1);
+    V_B1 = [V1_R;V1_theta;V1_phi];
+    r = vrrotvec(V_B1, V_I1);
+    Q = vrrotvec2mat(r);
+    
+    Q11(i) = Q(1, 1); 
+    Q12(i) = Q(1, 2); 
+    Q13(i) = Q(1, 3);
+    Q21(i) = Q(2, 1); 
+    Q22(i) = Q(2, 2); 
+    Q23(i) = Q(2, 3);
+    Q31(i) = Q(3, 1); 
+    Q32(i) = Q(3, 2); 
+    Q33(i) = Q(3, 3);
+end
 % Quaternion elements
-q1 = -(0.5*(1 + Q11 - Q22 - Q33).^0.5);
+q1 = +(0.5*(1 + Q11 - Q22 - Q33).^0.5);
 q2 = ((Q12 + Q21)./(4*q1));
 q3 = ((Q13 + Q31)./(4*q1));
 q4 = ((Q23 - Q32)./(4*q1));
 
-qn1 = [q1;q2;q3;q4];
+qn1 = [q1';q2';q3';q4'];
 test = sqrt(q1.^2+q2.^2+q3.^2+q4.^2);
+
 end
 
 
--- 3D_Two_stage_Rocket/lat_long_elev_azi_vec2.m
+++ 3D_Two_stage_Rocket/lat_long_elev_azi_vec2.m
@@ -1,13 +1,5 @@
 function [R2_I,V2_I,qn2] = lat_long_elev_azi_vec2(M,problem) 
 
-Elev_f = problem.Elev_f;
-Azim_f = problem.Azim_f;
-hf_f = problem.hf_f;
-Vf_f = problem.Vf_f;
-Elev_s = problem.Elev_s;
-Azim_s = problem.Azim_s;
-hf_s = problem.hf_s;
-Vf_s = problem.Vf_s;
 Re = problem.Re;
 nodes = problem.nodes;
 D = problem.D;
@@ -16,10 +8,10 @@ stage_time = 160;
 final_time = 1680;
 
 load time_alt_lat_long.csv
-time = time_alt_lat_long(10:35,1);
-alt = time_alt_lat_long(10:35,2)*1000;
-lat = time_alt_lat_long(10:35,3);
-long = time_alt_lat_long(10:35,4);
+time = time_alt_lat_long(:,1);
+alt = time_alt_lat_long(:,2)*1000;
+lat = time_alt_lat_long(:,3);
+long = time_alt_lat_long(:,4);
 
 t_2 = ((final_time-stage_time)/2).*nodes+(final_time+stage_time)/2;
 
@@ -27,8 +19,7 @@ Altitude0_2 = interp1(time, alt, t_2, 'pchip');
 latitude_2 = deg2rad(interp1(alt, lat, Altitude0_2, 'pchip'));
 longitude_2 = deg2rad(interp1(alt, long, Altitude0_2, 'pchip'));
 
-Elev_2 = linspace(Elev_s,Elev_f,M);
-Azim_2 = linspace(Azim_s,Azim_f,M);
+
 
 theta_2 = pi/2 - latitude_2;
 phi_2 = longitude_2;
@@ -39,49 +30,59 @@ Omega_x = 0;
 Omega_y = 0;
 Omega_I = [Omega_x; Omega_y; Omega_z];
 
-R2_B = (Altitude0_2' + Re);
-Rx2_I = R2_B' .* sin(theta_2) .* cos(phi_2);
-Ry2_I = R2_B' .* sin(theta_2) .* sin(phi_2);
-Rz2_I = R2_B' .* cos(theta_2);
+
+R2_B = (Altitude0_2 + Re);
+
+% R2_B = ((R2_B(end)-R2_B(1))/2).*nodes+(R2_B(end)+R2_B(1))/2;
+theta_2 = ((theta_2(end)-theta_2(1))/2).*nodes+(theta_2(end)+theta_2(1))/2;
+phi_2 = ((phi_2(end)-phi_2(1))/2).*nodes+(phi_2(end)+phi_2(1))/2;
+
+
+Rx2_I = R2_B .* sin(theta_2) .* cos(phi_2);
+Ry2_I = R2_B .* sin(theta_2) .* sin(phi_2);
+Rz2_I = R2_B .* cos(theta_2);
 
 R2_I = [Rx2_I'; Ry2_I'; Rz2_I'];
 R2_I_mag = sqrt(Rx2_I.^2 + Ry2_I.^2 + Rz2_I.^2);
 % display(R2_I_mag);
 
-% Velocity components in spherical coordinates
-V2_r = (2/(1680-160))*D*R2_B';
-V2_theta = 0;
-V2_phi = Omega_z*sin(theta_2)*(Re+hf_f);
-
-Vx2_I = V2_r.*sin(theta_2).*cos(phi_2) + V2_theta.*cos(theta_2).*cos(phi_2) - V2_phi.*sin(phi_2);
-Vy2_I = V2_r.*sin(theta_2).*sin(phi_2) + V2_theta.*cos(theta_2).*sin(phi_2) + V2_phi.*cos(phi_2);
-Vz2_I = V2_r.*cos(theta_2)-V2_theta.*sin(theta_2);
+Vx2_I = (2/(1680-160))*(D*Rx2_I);
+Vy2_I = (2/(1680-160))*(D*Ry2_I);
+Vz2_I = (2/(1680-160))*(D*Rz2_I);
 
 V2_I = [Vx2_I'; Vy2_I'; Vz2_I'];
 V2_I_mag = sqrt(Vx2_I.^2 + Vy2_I.^2 + Vz2_I.^2);
 % disp(V2_I_mag);
 
-latitude_2 = deg2rad(28);
-longitude_2 = deg2rad(0);
+Q11 = zeros(M, 1); 
+Q12 = zeros(M, 1);
+Q13 = zeros(M, 1);
+Q21 = zeros(M, 1); 
+Q22 = zeros(M, 1);
+Q23 = zeros(M, 1);
+Q31 = zeros(M, 1); 
+Q32 = zeros(M, 1);
+Q33 = zeros(M, 1);
 
-% Attitude matrix
-Q11 = - sin(latitude_2).*cos(longitude_2).*cos(Elev_2).*cos(Azim_2) - sin(longitude_2).*cos(Elev_2).*sin(Azim_2) + cos(latitude_2).*cos(longitude_2).*sin(Elev_2);
-
-Q12 = sin(latitude_2).*cos(longitude_2).*sin(Azim_2) - sin(longitude_2).*cos(Azim_2);
-
-Q13 = - sin(latitude_2).*cos(longitude_2).*sin(Elev_2).*cos(Azim_2) - sin(longitude_2).*sin(Elev_2).*sin(Azim_2) - cos(latitude_2).*cos(longitude_2).*cos(Elev_2);
-
-Q21 = - sin(latitude_2).*sin(longitude_2).*cos(Elev_2).*cos(Azim_2) + cos(longitude_2).*cos(Elev_2).*sin(Azim_2) + cos(latitude_2).*sin(longitude_2).*sin(Elev_2);
-
-Q22 = sin(latitude_2).*sin(longitude_2).*sin(Azim_2) + cos(longitude_2).*cos(Azim_2);
-
-Q23 = - sin(latitude_2).*sin(longitude_2).*sin(Elev_2).*cos(Azim_2) + cos(longitude_2).*sin(Elev_2).*sin(Azim_2) - cos(latitude_2).*sin(longitude_2).*cos(Elev_2);
-
-Q31 = cos(latitude_2).*cos(Elev_2).*cos(Azim_2) + sin(latitude_2).*sin(Elev_2);
-
-Q32 = - cos(latitude_2).*sin(Azim_2);
-
-Q33 = cos(latitude_2).*sin(Elev_2).*cos(Azim_2) - sin(latitude_2).*cos(Elev_2);
+for i = 1:M
+    V_I2 = [Vx2_I(i); Vy2_I(i); Vz2_I(i)];
+    V2_R = (2/(1680-160))*D(i,:)*R2_I_mag;
+    V2_theta = (2/(1680-160))*R2_I_mag(i).*(D(i,:)*theta_2);
+    V2_phi = (2/(1680-160))*R2_I_mag(i).*sin(theta_2(i)).*(D(i,:)*phi_2);
+    V_B2 = [V2_R;V2_theta;V2_phi];    
+    r = vrrotvec(V_B2, V_I2);
+    Q = vrrotvec2mat(r);
+    
+    Q11(i) = Q(1, 1); 
+    Q12(i) = Q(1, 2); 
+    Q13(i) = Q(1, 3);
+    Q21(i) = Q(2, 1); 
+    Q22(i) = Q(2, 2); 
+    Q23(i) = Q(2, 3);
+    Q31(i) = Q(3, 1); 
+    Q32(i) = Q(3, 2); 
+    Q33(i) = Q(3, 3);
+end
 
 % Quaternion elements
 q1 = -(0.5*(1 + Q11 - Q22 - Q33).^0.5);
@@ -89,9 +90,7 @@ q2 = ((Q12 + Q21)./(4*q1));
 q3 = ((Q13 + Q31)./(4*q1));
 q4 = ((Q23 - Q32)./(4*q1));
 
-qn2 = [q1;q2;q3;q4];
-
-
+qn2 = [q1';q2';q3';q4'];
 test = sqrt(q1.^2+q2.^2+q3.^2+q4.^2);
 end
 
